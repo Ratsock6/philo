384d34dfb92d0eec462ba849e6b6f497fa8ba97a
Antoine Allou Vincheneux <aallou-v@c1r7p4.42lehavre.fr>
2024-03-22 17:19:17 +0100

    Norm

--- src/philo_utils.c
+++ src/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: aallou-v <aallou-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 17:26:43 by aallou-v          #+#    #+#             */
-/*   Updated: 2024/03/22 16:42:40 by aallou-v         ###   ########.fr       */
+/*   Updated: 2024/03/22 17:18:18 by aallou-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,24 +21,28 @@ void	philo_print(t_philo *philo, t_states states)
 	if (!philo->is_finish)
 	{
 		if (states == FORK)
-			printf("\e[92m%5li Philosophers %10s[%3li] has taken a fork.\e[0m\n", t, philo->name, philo->id);
+			printf("\e[92m%5li Philosophers %9s[%3li] has taken a fork.\e[0m\n",
+				t, philo->name, philo->id);
 		else if (states == EAT)
-			printf("\e[32m%5li Philosophers %10s[%3li] is eating.\e[0m\n", t, philo->name, philo->id);
+			printf("\e[32m%5li Philosophers %9s[%3li] is eating.\e[0m\n",
+				t, philo->name, philo->id);
 		else if (states == SLEEP)
-			printf("\e[94m%5li Philosophers %10s[%3li] is sleeping.\e[0m\n", t, philo->name, philo->id);
+			printf("\e[94m%5li Philosophers %9s[%3li] is sleeping.\e[0m\n",
+				t, philo->name, philo->id);
 		else if (states == THINK)
-			printf("\e[33m%5li Philosophers %10s[%3li] is thinking.\e[0m\n", t, philo->name, philo->id);
+			printf("\e[33m%5li Philosophers %9s[%3li] is thinking.\e[0m\n",
+				t, philo->name, philo->id);
 		else if (states == DEAD)
-			printf("\e[31m%5li Philosophers %10s[%3li]died.\e[0m\n", t, philo->name, philo->id);
+			printf("\e[31m%5li Philosophers %10s[%3li]died.\e[0m\n",
+				t, philo->name, philo->id);
 	}
 	pthread_mutex_unlock(&philo->core->arg.write);
 }
 
-
 void	sleep_think(t_philo *philo)
 {
 	size_t	i;
-	
+
 	if (!is_dead(philo))
 	{
 		philo_print(philo, SLEEP);
